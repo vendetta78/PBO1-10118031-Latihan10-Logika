c81b2de3e8cada8aa74d4f5b3abe537945822a0f
vendetta78 <55823091+vendetta78@users.noreply.github.com>
2019-09-26 12:03:07 +0700

    Add files via upload

---
+++ PBO1-10118031-Latihan10-Logika/src/pbo1/pkg10118031/latihan10/logika/PBO110118031Latihan10Logika.java
@@ -0,0 +1,22 @@
+package pbo1.pkg10118031.latihan10.logika;
+
+/**
+ *
+ * @author Vendetta
+ * NAMA              : Rizki Aria Putra
+ * KELAS             : PBO1
+ * NIM               : 10118031
+ * Deskripsi Program : Program ini berisi program untuk menyatakan 
+ *                     logika true atau false
+ */
+public class PBO110118031Latihan10Logika {
+
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : "
+                + dewasa);
+    }
+
+}
